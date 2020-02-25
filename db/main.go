cb87a36db8b33b03ee22d75078b60e3e0c94725c
Xanonymous <trusaidlin@gmail.com>
2020-02-26 02:56:49 +0800

    show db contents by for loop.

--- db/main.go
+++ db/main.go
@@ -3,12 +3,13 @@ package db
 import (
 	"database/sql"
 	"github.com/Xanonymous-GitHub/restaurant-server/secretes"
-	"time"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 )
 
 type Restaurants struct {
 	Name     string
-	LastDate time.Time
+	LastDate string
 	Detail   string
 }
 
@@ -20,9 +21,25 @@ func (d Restaurants) GetList(token string) {
 		panic(err.Error())
 	}
 	//open the database on server.
-	db, err := sql.Open("mysql", sqlInfo["UserName"]+":"+sqlInfo["Password"]+"@localhost/restaurant")
+	db, err := sql.Open("mysql", sqlInfo["UserName"]+":"+sqlInfo["Password"]+"@tcp(localhost)/restaurant")
 	if err != nil {
 		panic(err.Error())
 	}
 	defer db.Close()
+	// Execute the query
+	results, err := db.Query("SELECT * FROM restaurant")
+	if err != nil {
+		panic(err.Error())
+	}
+	for results.Next() {
+		var restaurant Restaurants
+		err = results.Scan(&restaurant.Name, &restaurant.LastDate, &restaurant.Detail)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		// and then print out the tag's Name attribute
+		log.Printf(restaurant.Name)
+		log.Printf(restaurant.LastDate)
+		log.Printf(restaurant.Detail)
+	}
 }
--- server.go
+++ server.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "github.com/Xanonymous-GitHub/restaurant-server/db"
 
+func run() {
+	var tmp db.Restaurants
+	tmp.GetList("0202494c:0063")
+}
+
+func main() {
+	run()
 }
